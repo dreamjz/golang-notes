bc6c36009adf990479066c85633d34e02c81f5f5
kesa <2356216441@qq.com>
2021-11-23 20:17:38 +0800

    Feat:endless note

---
+++ endless-note/go.mod
@@ -0,0 +1,8 @@
+module endless-note
+
+go 1.16
+
+require (
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6 // indirect
+	github.com/gin-gonic/gin v1.7.6 // indirect
+)
---
+++ endless-note/quick-start/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/fvbock/endless"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main(){
+	router := gin.Default()
+	router.GET("ping", func(c *gin.Context) {
+		c.String(http.StatusOK,"pong")
+	})
+	err := endless.ListenAndServe(":9090",router)
+	if err != nil {
+		log.Fatal("listen and serve error:",err.Error())
+	}
+	log.Println("Server on 9090 stopped")
+}
