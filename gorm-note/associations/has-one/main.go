713ac575f76474caec206a2b8603cf97e288a904
kesa <2356216441@qq.com>
2021-12-02 07:35:38 +0800

    Feat:gorm note

--- gorm-note/associations/has-one/main.go
+++ gorm-note/associations/has-one/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"gorm-note/utils"
 	"log"
+	"strconv"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -9,13 +11,13 @@ import (
 )
 
 const (
-	DBName = "has-one.db"
+	DBName    = "has-one.db"
+	UserCount = 10
 )
 
 type User struct {
 	ID         uint `gorm:"primaryKey"`
 	Name       string
-	Age        int
 	CreditCard CreditCard
 }
 
@@ -31,6 +33,16 @@ func main() {
 	defer sqlDB.Close()
 
 	createTables(db)
+	createUsers(db, UserCount)
+
+	//query association
+	var creditCard CreditCard
+	db.Model(&User{ID: 1}).Association("CreditCard").Find(&creditCard)
+	utils.PrintRecord(creditCard)
+	// append association
+	// SELECT * FROM `credit_cards` WHERE `credit_cards`.`user_id` = 1
+	// INSERT INTO `credit_cards` (`number`,`user_id`) VALUES ("new_xxx",2) ON CONFLICT (`id`) DO UPDATE SET `user_id`=`excluded`.`user_id` RETURNING `id`
+	db.Model(&User{ID: 2}).Association("CreditCard").Append(&CreditCard{Number: "new_xxx"})
 }
 
 func initializeDB() *gorm.DB {
@@ -46,4 +58,19 @@ func initializeDB() *gorm.DB {
 func createTables(db *gorm.DB) {
 	db.AutoMigrate(&User{})
 	db.AutoMigrate(&CreditCard{})
+}
+
+func createUsers(db *gorm.DB, num int) {
+	var count int64
+	db.Model(&User{}).Count(&count)
+	if count > 0 {
+		return
+	}
+	users := make([]User, num)
+	for i := 0; i < num; i++ {
+		name := "user_" + strconv.Itoa(i)
+		creditCardNumber := "xxx" + strconv.Itoa(i)
+		users[i] = User{Name: name, CreditCard: CreditCard{Number: creditCardNumber}}
+	}
+	db.Create(&users)
 }
