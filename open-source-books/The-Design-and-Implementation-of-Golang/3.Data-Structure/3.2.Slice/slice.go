c20ee2bdb1ff873bed27df1870968ff3eaf1dd21
kesa <2356216441@qq.com>
2022-03-28 17:47:49 +0800

    Feat: Go 语言设计与实现 -- 切片实现原理

--- open-source-books/The-Design-and-Implementation-of-Golang/3.Data-Structure/3.2.Slice/slice.go
+++ open-source-books/The-Design-and-Implementation-of-Golang/3.Data-Structure/3.2.Slice/slice.go
@@ -1,7 +1,28 @@
 package slice
 
+import "fmt"
+
 func newSlice() []int {
 	arr := [3]int{1, 2, 3}
 	slice := arr[0:1]
 	return slice
+}
+
+func appendTest1() {
+	fmt.Println("------------------------ Int64 8 Byte ------------------------")
+	var arr []int64
+	arr = append(arr, 1, 2, 3, 4, 5)
+	fmt.Printf("Arr: %v, Len: %d, Cap: %d\n", arr, len(arr), cap(arr))
+	fmt.Println("------------------------ Int32 4 Byte ------------------------")
+	var arr2 []int32
+	arr2 = append(arr2, 1, 2, 3, 4, 5)
+	fmt.Printf("Arr: %v, Len: %d, Cap: %d\n", arr2, len(arr2), cap(arr2))
+	fmt.Println("------------------------ Int16 2 Byte ------------------------")
+	var arr3 []int16
+	arr3 = append(arr3, 1, 2, 3, 4, 5)
+	fmt.Printf("Arr: %v, Len: %d, Cap: %d\n", arr3, len(arr3), cap(arr3))
+	fmt.Println("------------------------ Int8 1 Byte ------------------------")
+	var arr4 []int8
+	arr4 = append(arr4, 1, 2, 3, 4, 5)
+	fmt.Printf("Arr: %v, Len: %d, Cap: %d\n", arr4, len(arr4), cap(arr4))
 }
--- open-source-books/The-Design-and-Implementation-of-Golang/3.Data-Structure/3.2.Slice/slice_test.go
+++ open-source-books/The-Design-and-Implementation-of-Golang/3.Data-Structure/3.2.Slice/slice_test.go
@@ -1,1 +1,7 @@
 package slice
+
+import "testing"
+
+func TestAppendTest1(t *testing.T) {
+	appendTest1()
+}
